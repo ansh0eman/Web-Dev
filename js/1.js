61b3ad65973915a48d0f6366207387fce485ad62
Anshuman <anshutube18@gmail.com>
2024-01-13 22:26:13 +0530

    bruh

---
+++ js/1.js
@@ -0,0 +1,9 @@
+let student = {
+    name: "Anshuman",
+    "Phone Number": "620XXXXXXX9",
+    "Marks": "8.78"
+}
+console.log(student,  typeof student);
+student.name = "Ram";
+student["Phone Number"]= "829293";
+console.log(student,  typeof student);
